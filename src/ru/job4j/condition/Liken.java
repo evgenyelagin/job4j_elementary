4a06da16be4f7ded40d7c2ea45c2edeafa5169d3
evgenyelagin <anykeysky@google.com>
2020-05-30 08:37:31 +0300

    0. Операции сравнения[235631#293246]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+        boolean result1 = first > second;
+        boolean result2 = first < second;
+        boolean result3 = first == second;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+    }
