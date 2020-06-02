d5025b342f187a5d2412ccd73ef1ae9c4a4a4f54
evgenyelagin <anykeysky@google.com>
2020-06-02 08:20:19 +0300

    2. Тесты для идеальный вес[159458#293318]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -10,14 +10,14 @@ public class FitTest {
     @Test
     public void manWeight() {
         double in = 176;
-        double expected = 87.39999999999999;
+        double expected = 87.40;
         double out = Fit.manWeight(in);
         Assert.assertEquals(expected, out,0.01);
     }
     @Test
     public void womanWeight() {
         double in = 165;
-        double expected = 63.24999999999999;
+        double expected = 63.25;
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out,0.01);
     }
