202cc967eac2644eb281609ac702faa7b2485fc3
evgenyelagin <anykeysky@google.com>
2020-06-02 08:15:06 +0300

    1.1. Входные данные в тесте[297750#297753]

--- test/ru/job4j/calculator/X2Test.java
+++ test/ru/job4j/calculator/X2Test.java
@@ -6,15 +6,43 @@ import org.junit.Test;
 public class X2Test {
 
         @Test
-        public void calc() {
-            /* Входные параметры. Их будет 4 штуки */
+        public void calc1() {
             int a = 1;
             int b = 1;
             int c = 1;
             int x = 1;
-            /* ожидаемое значение. Это всегда одна переменная. */
             int expected = 3;
             int rsl = X2.calc(a, b, c, x);
             Assert.assertEquals(rsl, expected);
         }
+    @Test
+    public void calc2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(rsl, expected);
+    }
+    @Test
+    public void calc3() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(rsl, expected);
+    }
+    @Test
+    public void calc4() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(rsl, expected);
+    }
     }
