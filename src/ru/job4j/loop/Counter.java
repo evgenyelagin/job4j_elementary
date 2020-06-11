b4b68c36f52850b20af2b40502d8c0d0c20e580b
evgenyelagin <anykeysky@google.com>
2020-06-11 11:25:35 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[192#293339]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -9,9 +9,24 @@ public class Counter {
         return sum;
     }
 
+
+    public static int sumByEven(int start, int finish) {
+        int sum1 = 0;
+        for (int a = start; a <= finish; a++) {
+            if (a % 2 == 0) {
+                sum1 = sum1 + a;
+            }
+        }
+        return sum1;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
