46235bd3d2fe7a4c0dae4cfa250b9e3644a2092e
evgenyelagin <anykeysky@google.com>
2020-06-14 07:05:09 +0300

    5.4. Протеиновая диета.[156313#293331] тест

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,14 +3,14 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
             month++;
                    }
         return month;
             }
-       public static void main(String[] args) {
+            public static void main(String[] args) {
             System.out.println(calc(90, 100));
         }
     }
