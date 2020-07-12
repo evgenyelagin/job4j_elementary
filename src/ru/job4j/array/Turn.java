58eda6c4dc630d6e31c3c9332e4c93ef13df01b4
evgenyelagin <anykeysky@google.com>
2020-07-12 16:04:22 +0300

    6.2.3. Перевернуть массив.[226790#293272]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -2,37 +2,18 @@ package ru.job4j.array;
 
 public class Turn {
     public static void main(String[] args) {
-        int[] input = new int[]{1, 2, 3, 4, 5};
+        int[] input = new int[]{1, 2, 3, 4};
         int[] rsl = back(input);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
         }
     }
-
     public static int[] back(int[] input) {
-        for (int i = input.length - 1; i > 0; i--) {
-            if (i == 1) {
-                int temp = input[0];
-                input[0] = input[input.length - 1];
-                input[input.length - 1] = temp;
-                break;
-            }
-            if (i == 2) {
-                int temp1 = input[0];
-                input[0] = input[input.length - 1];
-                input[input.length - 1] = temp1;
-                break;
-            }
-            if (i == 3) {
-                int temp1 = input[0];
-                input[0] = input[input.length - 1];
-                input[input.length - 1] = temp1;
-                int temp2 = input[1];
-                input[1] = input[input.length - 2];
-                input[input.length - 2] = temp2;
-                               break;
-            }
-        }
+        for (int i = 0; i < input.length - 1 - i; i++) {
+                int temp = input[i];
+                input[i] = input[input.length - 1 - i];
+                input[input.length - 1 - i] = temp;
+                          }
         return input;
     }
     }
