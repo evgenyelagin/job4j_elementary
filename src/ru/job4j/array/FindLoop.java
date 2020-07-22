442baa238aa3f17a29c2f1c3d274c215cf642958
evgenyelagin <anykeysky@google.com>
2020-07-22 16:44:45 +0300

    6.5. Поиск индекса в диапазоне.[156318#293262]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -11,12 +11,26 @@ package ru.job4j.array;
             }
             return rst;
         }
+        public static int indexOf(int[] data, int el, int start, int finish) {
+            int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+            for (int index = start; index < finish; index++) {
+                if (data[index] == el) {
+                   rst = index;
+                   break;
+                }
+            }
+            return rst;
+        }
         public static void main(String[] args) {
             int[] input = new int[] {5, 4, 3, 2, 1, 0};
             int rsl = indexOf(input, 5);
             System.out.println(rsl);
+            int[] data = new int[] {5, 2, 10, 2, 4};
+            int rslOne = indexOf(data, 1, 2, 4);
+            System.out.println(rslOne);
             }
         }
+
 
 
 
