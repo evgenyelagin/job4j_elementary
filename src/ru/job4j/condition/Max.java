275e0d183a05f239f21324cc899e4af5f386992d
evgenyelagin <anykeysky@google.com>
2020-12-10 21:44:33 +0300

    3. Перегрузить метод max для трех чисел[122644#293422] v.1

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,11 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
-            }
+    public static int max(int one, int two, int three, int four) {
+        int tmp1 = max(one, two);
+        int tmp2 = max(three, four);
+        return max(tmp1, tmp2);
+    }
+
+    public static int max(int one, int two) {
+        return Math.max(one, two);
+    }
+
     public static void main(String[] args) {
-        int result = Max.max(1, 2);
+        int result = Max.max(8, 400, 1, 1026);
         System.out.println(result);
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,19 +8,43 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
 
     @Test
-    public void whenMaxRight() {
-        int result = Max.max(1, 2);
-        assertThat(result, is(2));
+    public void whenMaxOne() {
+        int result = Max.max(10, 20);
+        assertThat(result, is(20));
     }
     @Test
-    public void whenMaxLeft() {
+    public void whenMaxTwo() {
         int result = Max.max(2, 1);
         assertThat(result, is(2));
     }
     @Test
-    public void whenLeftEquallyRight() {
+    public void whenOneEquallyTwo() {
         int result = Max.max(2, 2);
         assertThat(result, is(2));
     }
-
+    @Test
+    public void whenMaxFour() {
+        int result = Max.max(2, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenMaxOneOfFour() {
+        int result = Max.max(100, 2, 3, 4);
+        assertThat(result, is(100));
+    }
+    @Test
+    public void whenMaxThree() {
+        int result = Max.max(100, 2, 300, 4);
+        assertThat(result, is(300));
+    }
+    @Test
+    public void whenMaxTwoOfFour() {
+        int result = Max.max(100, 1200, 300, 4);
+        assertThat(result, is(1200));
+    }
+    @Test
+    public void whenOneOfFourEquallyFour() {
+        int result = Max.max(1000, 120, 300, 1000);
+        assertThat(result, is(1000));
+    }
 }
